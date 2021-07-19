a9a4c5130eea5c6cddf7fcc8b82c8fc1d0e23957
arch2 <mattstrattn@gmail.com>
2021-07-19 13:28:58 -0400

    small tweaks

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default-dark.css'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(VueMaterial)
+    .use(VueAxios, axios)    
+    .mount('#app')
