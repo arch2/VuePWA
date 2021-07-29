8c991d22c32a111541eb379c996c1a3b28766760
arch2 <mattstrattn@gmail.com>
2021-07-29 15:12:44 -0400

    add simple location route

---
+++ src/views/Location.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="Location">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <p>Location route</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  components: {},
+})
+export default class Location extends Vue {}
+</script>
