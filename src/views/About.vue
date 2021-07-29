9c0c4ffe8c19f7633a7ea2a3645be571b0962573
arch2 <mattstrattn@gmail.com>
2021-07-29 13:41:59 -0400

    works?

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,46 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="hello">
+    <StreamBarcodeReader
+      @decode="(a, b, c) => onDecode(a, b, c)"
+      @loaded="() => onLoaded()"
+    ></StreamBarcodeReader>
+    Input Value: {{ text || "Nothing" }}
   </div>
 </template>
+
+<script>
+import { StreamBarcodeReader } from "vue-barcode-reader";
+
+export default {
+  name: "HelloWorld",
+  components: {
+    StreamBarcodeReader,
+  },
+  data() {
+    return {
+      text: "",
+      id: null,
+    };
+  },
+  props: {
+    msg: String,
+  },
+  methods: {
+    onDecode(a, b, c) {
+      console.log(a, b, c);
+      this.text = a;
+      if (this.id) clearTimeout(this.id);
+      this.id = setTimeout(() => {
+        if (this.text === a) {
+          this.text = "";
+        }
+      }, 5000);
+    },
+    onLoaded() {
+      console.log("load");
+    },
+  },
+};
+</script>
+<style scoped>
+</style>
